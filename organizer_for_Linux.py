7c9b3360cb2c56f59308e64afae2872f16fb349f
kirakbol <61711367+kirakbol@users.noreply.github.com>
2021-08-03 19:04:38 +0300

    Made changes with dictionaries according to TasosFrago's method

--- organizer_for_Linux.py
+++ organizer_for_Linux.py
@@ -1,6 +1,8 @@
 import shutil
 import glob
 import os
+'''
+#previously:
 
 document_dir = '/home/pi/Documents/'
 audio_dir = '/home/pi/Music/'
@@ -12,7 +14,7 @@ document_extensions = ['.doc', '.docx', '.log', '.msg', '.odt', '.pages', '.rtf'
 audio_extensions = ['.aif', '.aac', '.iff', '.m3u', '.m4a', '.mid', '.mp3', '.mpa', '.wav', '.wma']
 video_extensions = ['.3g2', '.3gp', '.asf', '.avi', '.flv', '.m4v', '.mov', '.mp4', '.mpg', '.rm', '.srt', '.swf', '.vob', '.wmv']
 picture_extensions = ['.3dm', '.3ds', '.max', '.obj', '.bmp', '.dds', '.gif', '.heic', '.jpg', '.png', '.psp', '.pspimage', '.tga', '.thm', '.tif', '.tiff', '.yuv', '.ai', '.eps', '.svg']
-python_extensions = ['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml']
+pythonscript_extensions = ['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml']
 
 for file in glob.glob(f'/home/pi/Downloads/*{document_extensions}'):
     shutil.move(file, document_dir)
@@ -30,6 +32,29 @@ for file in glob.glob(f'/home/pi/Downloads/*{picture_extensions}'):
     shutil.move(file, picture_dir)
     print(f'moved {file} to {picture_dir}')
 
-for file in glob.glob(f'/home/pi/Downloads/*{python_extensions}'):
+for file in glob.glob(f'/home/pi/Downloads/*{pythonscript_extensions}'):
     shutil.move(file, pythonscripts_dir)
     print(f'moved {file} to {pythonscripts_dir}')
+'''
+
+#current
+types=[{"name":"document",
+"path":"/home/pi/Documents/",
+"extensions":['.doc', '.docx', '.log', '.msg', '.odt', '.pages', '.rtf', '.tex', '.txt', '.wpd', '.wps']},
+{"name":"audio",
+"path":"/home/pi/Music/",
+"extensions":['.aif', '.aac', '.iff', '.m3u', '.m4a', '.mid', '.mp3', '.mpa', '.wav', '.wma']},
+{"name":"video",
+"path":"/home/pi/Videos/",
+"extensions":['.3g2', '.3gp', '.asf', '.avi', '.flv', '.m4v', '.mov', '.mp4', '.mpg', '.rm', '.srt', '.swf', '.vob', '.wmv']},
+{"name":"picture",
+"path":"/home/pi/Pictures/",
+"extensions":['.3dm', '.3ds', '.max', '.obj', '.bmp', '.dds', '.gif', '.heic', '.jpg', '.png', '.psp', '.pspimage', '.tga', '.thm', '.tif', '.tiff', '.yuv', '.ai', '.eps', '.svg']},
+{"name":"pythonscript",
+"path":"/home/pi/Python/",
+"extensions":['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml']}]
+
+for type in types:
+    for file in glob.glob(f'/home/pi/Downloads/*{type["extensions"]}'):
+        shutil.move(file, type["path"])
+        print(f'moved {file} to {type["path"]}')
